cffa685ab74471088142d04599463fb3c9c93d15
slawek <slawekwch@gmail.com>
2022-01-13 11:01:38 +0100

    API Sciezki parametry zmiana nazwy klasy startowej

--- api-sciezki-parametry-1/src/main/java/pl/javastart/healtycalc/HealthyCalcApplication.java
+++ api-sciezki-parametry-1/src/main/java/pl/javastart/healtycalc/HealthyCalcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiSciezkiParametry1Application {
+public class HealthyCalcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiSciezkiParametry1Application.class, args);
+		SpringApplication.run(HealthyCalcApplication.class, args);
 	}
 
 }
