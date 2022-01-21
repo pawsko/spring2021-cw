d6556355f1c3a1448e60373b0ada3e1bc23b434c
Slawek Ludwiczak <slawek@javastart.pl>
2022-01-21 13:49:21 +0100

    API CRUD skracacz linkw cw2 aktualizacja z haslem

---
+++ api-crud-2-aktualizacja/src/main/java/pl/javastart/shortener/redirect/RedirectController.java
@@ -0,0 +1,30 @@
+package pl.javastart.shortener.redirect;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.javastart.shortener.link.LinkService;
+import pl.javastart.shortener.link.dto.LinkDto;
+
+import java.net.URI;
+
+@Controller
+class RedirectController {
+    private final LinkService linkService;
+
+    public RedirectController(LinkService linkService) {
+        this.linkService = linkService;
+    }
+
+    @GetMapping("/redir/{id}")
+    ResponseEntity<?> redirect(@PathVariable String id) {
+        return linkService.incrementVisitsById(id)
+                .map(LinkDto::getTargetUrl)
+                .map(targetUrl -> ResponseEntity
+                        .status(302)
+                        .location(URI.create(targetUrl))
+                        .build())
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
